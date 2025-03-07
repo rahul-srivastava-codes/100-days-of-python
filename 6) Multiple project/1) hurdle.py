d854cd6813ce4cdadc6fc3fab9d128afbb51c45f
Rahul Srivastava <117383410+rahul-srivastava-codes@users.noreply.github.com>
2025-03-07 18:32:12 +0530

    Create 1) hurdle.py

---
+++ 6) Multiple project/1) hurdle.py
@@ -0,0 +1,21 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def crossing():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for i in range(0,6):
+    crossing()
+
+
+
+# copy this link on browser and later copy this code
